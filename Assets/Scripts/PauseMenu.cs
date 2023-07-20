fbcff63eb4a2be99898579f8355d4750c81677c2
lmcunneen <liam.cunneen282@gmail.com>
2023-07-20 23:13:39 +1000

    Added Pause Menu
     - Pause menu stops time, but doesn't have any settings or exit buttons. It also doesn't prevent attack inputs, which makes the player pre-emptively attack. This will be fixed tomorrow

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PauseMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public GameObject gameUIObject;
+    public GameObject pauseScreenObject;
+
+    private bool menuOpen;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameUIObject.SetActive(true);
+        pauseScreenObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!menuOpen)
+            {
+                menuOpen = true;
+                gameUIObject.SetActive(false);
+                pauseScreenObject.SetActive(true);
+                Time.timeScale = 0f;
+                audioMixer.SetFloat("MusicLowpass", 875.00f);
+
+                //To Do: Disable all player input during pause (by adding if !menuOpen to input conditional statements)
+            }
+
+            else
+            {
+                menuOpen = false;
+                gameUIObject.SetActive(true);
+                pauseScreenObject.SetActive(false);
+                Time.timeScale = 1f;
+                audioMixer.SetFloat("MusicLowpass", 22000.00f);
+            }
+        }
+    }
+}
